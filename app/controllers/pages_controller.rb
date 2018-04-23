81e0d59f0fe4f38accb0cc640881dcc7f04e02f4
Aaron Harrison <aaronharrison16@gmail.com>
2018-04-22 19:26:03 -0600

    Implemented features outlined for project one.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def qoutes
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :guides
+  get 'pages/qoutes'
+
+  get 'pages/home'
+
+
+  root to: 'pages#home'
+
 end
